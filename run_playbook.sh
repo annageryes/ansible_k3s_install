b89ccf63f6154a77c0bbc3e616818602dca348d7
annageryes <168434365+annageryes@users.noreply.github.com>
2024-10-29 18:42:58 +0200

    Update run_playbook.sh

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+################################################
+# Created by Anna Sakran
+# Purpose: trigger the ansible playbook with the right flags
+# Version: 0.0.1
+# Date: 20.08.2024
+#################################################
 ansible-playbook main-playbook.yml -K -k
